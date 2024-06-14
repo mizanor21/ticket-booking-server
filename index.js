9e4aba650985507b031954c618ea58effae99575
mizan21 <mizanorrahman398@gmail.com>
2024-06-14 20:17:33 +0600

    server setup

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Ticket booking server is running");
 });
 
 app.listen(port, () => {
